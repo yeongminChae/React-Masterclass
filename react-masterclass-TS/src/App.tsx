181382a9adf9b2e8a57116b667e629a9cd0d9b5f
Yeongmin_Chae <codudals98@naver.com>
2022-07-11 17:06:34 +0900

    chapter 2 : last lecture -> recap for the typescript in react

--- react-masterclass-TS/src/App.tsx
+++ react-masterclass-TS/src/App.tsx
@@ -8,10 +8,23 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text} </H1>;
+}
+
 function App() {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
     <Container>
-      <H1>Protected</H1>
+      <Dummy active text="Hello" />
+      <form>
+        <button onClick={onClick}>click me</button>
+      </form>
+      {/* active == active=true */}
     </Container>
   );
 }
