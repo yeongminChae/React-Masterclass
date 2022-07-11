84589ea3126e50cebc2e6439e6ae8f24a57e1d7e
Yeongmin_Chae <codudals98@naver.com>
2022-07-11 11:00:59 +0900

    themes in Styled Components the last section in chapter1 : STYLED COMPONENTS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import styled, { keyframes } from "styled-components";
 
 const Title = styled.h1`
-  color: tomato;
-  &:hover {
-    color: teal;
-  }
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Wrapper = styled.div`
@@ -13,61 +10,13 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
-  /* h1 {
-    color: tomato;
-    &:hover {
-      color: teal;
-    }
-  } */
-  ${Title}:hover {
-    font-size: 99px;
-  }
-`;
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Btn = styled.button`
-  color: tomato;
-`;
-
-const Input = styled.input.attrs({ required: true, maxLength: 10 })`
-  background-color: tomato;
-`;
-
-const anim = keyframes`
-  from {
-    color: tomato;
-  }
-  to {
-    color: teal;
-  }
-`;
-const BtnAnim = styled.button`
-  animation: ${anim} 0.5s infinite;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      {/* <Box bgColor="teal" />
-      <Circle bgColor="tomato" /> */}
-      {/* <Btn>Log In</Btn>
-      <Btn as="a" href="/">
-        Go home
-      </Btn> */}
-      {/* <Input />
-      <Input />
-      <Input /> */}
-      {/* <BtnAnim>hello</BtnAnim> */}
-      <Title>Hello to Super Recap</Title>
+      <Title>Hello to be Here</Title>
     </Wrapper>
   );
 }
