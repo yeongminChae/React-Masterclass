8af09fa040e3287daf62ed0598ff3edbd6ec0682
Yeongmin_Chae <codudals98@naver.com>
2022-07-11 15:52:17 +0900

    chapter 2 : Optional Props

--- react-masterclass-TS/src/Circle.tsx
+++ react-masterclass-TS/src/Circle.tsx
@@ -2,20 +2,34 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
+
 const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string; // '?' mean is optional
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({
+  bgColor,
+  borderColor,
+  text = "this is default value",
+}: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {/* borderColor={borderColor ?? bgColor}  -> if there is no bordercolor's value , that color will be same with bgColor */}
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
