787a93d640029318542221b70f96f1390a5dbe43
Yeongmin_Chae <codudals98@naver.com>
2022-07-11 10:02:12 +0900

    'As' and Attrs in styled components

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
-
-const Circle = styled(Box)`
-  // i can inheritance the Box's styles
-  // background-color: ${(props) => props.bgColor};
-  // width: 100px;
-  // height: 100px;
-
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      {/* <Btn>Log In</Btn>
+      <Btn as="a" href="/">
+        Log In
+      </Btn> */}
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
