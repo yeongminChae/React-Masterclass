67895a6fe64347e9be6f09a25525c9daa56103e1
Yeongmin_Chae <codudals98@naver.com>
2022-07-11 10:47:10 +0900

    Super Recap for Styled Components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,73 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: tomato;
+  &:hover {
+    color: teal;
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-const rotateAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px
+  /* h1 {
+    color: tomato;
+    &:hover {
+      color: teal;
+    }
+  } */
+  ${Title}:hover {
+    font-size: 99px;
   }
-  50%{
-    transform:rotate(360deg);
-    border-radius:100px
-  }
-  100% {
-    transform:rotate(0deg);
-    border-radius:0px
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 30px;
 `;
 
 const Box = styled.div`
-  height: 200px;
-  width: 200px;
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Btn = styled.button`
+  color: tomato;
+`;
+
+const Input = styled.input.attrs({ required: true, maxLength: 10 })`
   background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimation} 2s linear infinite;
-  ${Emoji}:hover {
-    font-size: 40px;
+`;
+
+const anim = keyframes`
+  from {
+    color: tomato;
   }
+  to {
+    color: teal;
+  }
+`;
+const BtnAnim = styled.button`
+  animation: ${anim} 0.5s infinite;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🍌</Emoji>
-      </Box>
+      {/* <Box bgColor="teal" />
+      <Circle bgColor="tomato" /> */}
+      {/* <Btn>Log In</Btn>
+      <Btn as="a" href="/">
+        Go home
+      </Btn> */}
+      {/* <Input />
+      <Input />
+      <Input /> */}
+      {/* <BtnAnim>hello</BtnAnim> */}
+      <Title>Hello to Super Recap</Title>
     </Wrapper>
   );
 }
