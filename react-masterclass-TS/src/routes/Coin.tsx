10b2bb7121a34c79a138aecad2ab884e00a577a5
Yeongmin_Chae <codudals98@naver.com>
2022-07-12 22:27:58 +0900

    chapter 3 : Data Types ( teach to typecript for which types of we using in the interface )

--- react-masterclass-TS/src/routes/Coin.tsx
+++ react-masterclass-TS/src/routes/Coin.tsx
@@ -32,20 +32,89 @@ interface Routeparams {
 interface RouteState {
   name: string;
 }
+
+interface InfoData {
+  // click the right button on the console.log(infoData)
+  // and press the button which is "save the object as a global variable"
+  // and write this code in console "Object.keys(temp1).join()"
+  // join is making value as a string
+  // Object.values(temp1).map(v => typeof v).join()
+  // with the map function explict types of the values
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  //   tags: object; in this design we don't need the object
+  //   team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  //   links: object;
+  //   links_extended: object;
+  //   whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<Routeparams>();
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
   useEffect(() => {
     (async () => {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
+      console.log(infoData);
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
+      console.log(priceData);
       setInfo(infoData);
       setPriceInfo(priceData);
     })();
@@ -55,7 +124,7 @@ function Coin() {
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : <span>null</span>}
+      {loading ? <Loader>Loading...</Loader> : null}
     </Container>
   );
 }
