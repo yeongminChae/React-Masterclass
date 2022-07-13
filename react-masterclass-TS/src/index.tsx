fbaa60f25cb39bae833b353ede4421a997cb3612
Yeongmin_Chae <codudals98@naver.com>
2022-07-13 09:55:24 +0900

    chapter 3 : React Query part One

--- react-masterclass-TS/src/index.tsx
+++ react-masterclass-TS/src/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import { Theme } from "./theme";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={Theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={Theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
