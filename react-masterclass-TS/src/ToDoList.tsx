7c3e8fb9a4e91dfb0be19460952e7f66cdeae459
Yeongmin_Chae <codudals98@naver.com>
2022-07-17 00:44:20 +0900

    chapter 4 : basic set up for implementing the ToDo list

--- react-masterclass-TS/src/ToDoList.tsx
+++ react-masterclass-TS/src/ToDoList.tsx
@@ -1,125 +1,28 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-// without react-hook-form
-
-// function ToDoList() {
-//   const [ToDo, setToDo] = useState("");
-//   const [ToDoError, setToDoError] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDoError("");
-//     setToDo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     if (ToDo.length < 10) {
-//       return setToDoError("ToDo shoudl be longer");
-//     }
-//     console.log("submit");
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={ToDo} placeholder="Write a To Do" />
-//         <button>Add </button>
-//         {ToDoError != "" ? ToDoError : ""}
-//       </form>
-//     </div>
-//   );
-// }
-
-// with react-hook-form
-
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError: string;
+  toDo: string;
 }
+
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({ defaultValues: { email: "@naver.com" } });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "Password r not the same" },
-        { shouldFocus: true }
-      );
-    }
-    // setError("extraError", { message: "server offline" });
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const hadleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
-  console.log(errors);
   return (
     <div>
-      {" "}
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        {" "}
+      <form onSubmit={handleSubmit(hadleValid)}>
         <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed",
-            },
+          {...register("toDo", {
+            required: "Please write ToDOs",
           })}
-          placeholder="Email"
-        />{" "}
-        <span>{errors?.email?.message}</span>{" "}
-        <input
-          {...register("firstName", {
-            required: "write here",
-            validate: {
-              Nonico: (value) =>
-                value.includes("nico") ? "nico not allowed" : true,
-              NoNick: (value) =>
-                value.includes("nick") ? "nick not allowed" : true,
-            },
-          })}
-          placeholder="First Name"
-        />{" "}
-        <span>{errors?.firstName?.message}</span>{" "}
-        <input
-          {...register("lastName", {
-            required: "write here",
-          })}
-          placeholder="Last Name"
-        />{" "}
-        <span>{errors?.lastName?.message}</span>{" "}
-        <input
-          {...register("username", { required: "write here", minLength: 5 })}
-          placeholder="Username"
-        />{" "}
-        <span>{errors?.username?.message}</span>{" "}
-        <input
-          {...register("password", { required: "write here", minLength: 5 })}
-          placeholder="Password"
-        />{" "}
-        <span>{errors?.password?.message}</span>{" "}
-        <input
-          {...register("password1", {
-            required: "Password is required",
-            minLength: { value: 5, message: "Your password is too short." },
-          })}
-          placeholder="Password1"
-        />{" "}
-        <span>{errors?.password1?.message}</span>
-        <button>Add</button> <span>{errors?.extraError?.message}</span>
-      </form>{" "}
+          placeholder="Write a To Do"
+        />
+        <button>Add </button>
+      </form>
     </div>
   );
 }
+
 export default ToDoList;
