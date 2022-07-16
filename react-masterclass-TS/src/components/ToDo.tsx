16870eb4e63dcb716e006c3bc84b3e22e94bd80e
Yeongmin_Chae <codudals98@naver.com>
2022-07-17 01:47:31 +0900

    chapter 4 : Immutability part 1

--- react-masterclass-TS/src/components/ToDo.tsx
+++ react-masterclass-TS/src/components/ToDo.tsx
@@ -1,31 +1,34 @@
+import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-  //   const onClick = (newCategory: IToDo["category"]) => {};
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      return oldToDos;
+    });
   };
   return (
     <li>
       <span>{text}</span>
       {category !== "DOING" && (
-        // <button onClick={() => onClick("DOING")}>Doing</button>
         <button name="DOING" onClick={onClick}>
           Doing
         </button>
       )}
       {category !== "TODO" && (
-        // <button onClick={() => onClick("TODO")}>ToDo</button>
         <button name="TODO" onClick={onClick}>
           ToDo
         </button>
       )}
       {category !== "DONE" && (
-        // <button onClick={() => onClick("DONE")}>Done</button>
         <button name="DONE" onClick={onClick}>
           Done
         </button>
