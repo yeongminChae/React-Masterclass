68ed7000182d28d1baa23bd7d2f43c55d89902f9
Yeongmin_Chae <codudals98@naver.com>
2022-07-17 01:00:58 +0900

    chapter 4 : Add and print ToDos

--- react-masterclass-TS/src/App.tsx
+++ react-masterclass-TS/src/App.tsx
@@ -1,5 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components";
-import ToDoList from "./ToDoList";
+import ToDoList from "./components/ToDoList";
 
 const Globalstyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
