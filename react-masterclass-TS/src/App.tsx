3628b6de5576acf70e8982e98f17510c757c4d25
Yeongmin_Chae <codudals98@naver.com>
2022-07-19 22:58:12 +0900

    chapter 6 : Basic Animations

--- react-masterclass-TS/src/App.tsx
+++ react-masterclass-TS/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -7,19 +8,25 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const Box = styled.div`
+
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
 function App() {
   return (
     <Wrapper>
-      {" "}
-      <Box /> <div></div> <motion.div></motion.div>{" "}
+      <Box
+        transition={{ type: "spring", delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 }
+
 export default App;
