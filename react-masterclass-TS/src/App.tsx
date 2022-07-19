533ac835619b8db38486df2502681c58fcdcdb76
Yeongmin_Chae <codudals98@naver.com>
2022-07-19 23:23:23 +0900

    chapter 6 : Variants Part 2

--- react-masterclass-TS/src/App.tsx
+++ react-masterclass-TS/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -7,22 +8,64 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.2);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  place-self: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
 };
+
+const cicleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      {" "}
-      <Box variants={myVars} initial="start" animate="end" />{" "}
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={cicleVariants} />
+        <Circle variants={cicleVariants} />
+        <Circle variants={cicleVariants} />
+        <Circle variants={cicleVariants} />
+      </Box>
     </Wrapper>
   );
 }
