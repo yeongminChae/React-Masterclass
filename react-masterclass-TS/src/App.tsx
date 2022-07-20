50bb7a6000fe352ec36951156890e5ae7a653b4d
Yeongmin_Chae <codudals98@naver.com>
2022-07-20 10:12:47 +0900

    chapter 6 : Motion value part 2

--- react-masterclass-TS/src/App.tsx
+++ react-masterclass-TS/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
@@ -20,13 +20,14 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  // useEffect(() => {
-  //   x.onChange(() => console.log(x.get()));
-  // }, [x]);
+  const potato = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  useEffect(() => {
+    // x.onChange(() => console.log(x.get()));
+    potato.onChange(() => console.log(potato.get()));
+  }, [x]);
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>click me</button>
-      <Box style={{ x }} drag="x" dragSnapToOrigin />
+      <Box style={{ x, scale: potato }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
