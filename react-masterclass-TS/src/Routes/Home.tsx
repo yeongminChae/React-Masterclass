79a3e770fff4b9eaeb64d2d8fe0a9e22a9f16479
Yeongmin_Chae <codudals98@naver.com>
2022-07-22 15:54:58 +0900

    chapter 7 : Nomflix clone Slider part 1 < make a slider component : put a animation >

--- react-masterclass-TS/src/Routes/Home.tsx
+++ react-masterclass-TS/src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../Api";
@@ -35,21 +37,80 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  margin-bottom: 5px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 64px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+    // +- 10 is for making gap between old animation and new animation
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
+
+// window.innerWidth
+// window.outerWidth
+// these functions help us to get my window's width
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex((prev) => prev + 1);
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title} </Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                transition={{ type: "tween", duration: 1 }}
+                exit="exit"
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
